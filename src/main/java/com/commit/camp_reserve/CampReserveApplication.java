75f71460fdd4bc4bf5db7aeccb7d16c7eea5d67e
PCH <paekrng7@naver.com>
2024-07-08 13:35:33 +0900

    init: spring setting

---
+++ src/main/java/com/commit/camp_reserve/CampReserveApplication.java
@@ -0,0 +1,13 @@
+package com.commit.camp_reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commit/camp_reserve/CampReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commit.camp_reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
