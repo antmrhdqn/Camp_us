4e7ebef05cabcce4b000b53af44630a431ccc992
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-15 17:10:12 +0900

    Feat: 회원가입 기능 추가

--- src/main/java/com/commit/camp_reserve/service/UserServiceImpl.java
+++ src/main/java/com/commit/camp_reserve/service/UserServiceImpl.java
@@ -1,9 +1,34 @@
 package com.commit.camp_reserve.service;
 
+import com.commit.camp_reserve.dto.SignUpUserInfo;
+import com.commit.camp_reserve.entity.User;
+import com.commit.camp_reserve.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }가
+
+    @Transactional
+    @Override
+    public void signUpUser(SignUpUserInfo userInfo) {
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        User user = modelMapper.map(userInfo, User.class);
+        userRepository.save(user);
+    }
 }
