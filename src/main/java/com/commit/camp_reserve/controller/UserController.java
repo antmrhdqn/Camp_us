2e288a0e39619105efecca1a78f551e6041c9c71
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-15 16:19:18 +0900

    Feat: user관련 컨트롤러 기본 세팅

---
+++ src/main/java/com/commit/camp_reserve/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.commit.camp_reserve.controller;
+
+import com.commit.camp_reserve.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}
