38be433dbca6b844481e78ec4533cd34e3257bf9
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-15 17:09:22 +0900

    Feat: 회원가입기능 추가

--- src/main/java/com/commit/camp_reserve/controller/UserController.java
+++ src/main/java/com/commit/camp_reserve/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.commit.camp_reserve.controller;
 
+import com.commit.camp_reserve.dto.SignUpUserInfo;
 import com.commit.camp_reserve.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +20,10 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("sign-up")
+    public ResponseEntity<Void> signUpUser(@RequestBody SignUpUserInfo userInfo) {
+        userService.signUpUser(userInfo);
+        return ResponseEntity.ok().build();
+    }
 
 }
