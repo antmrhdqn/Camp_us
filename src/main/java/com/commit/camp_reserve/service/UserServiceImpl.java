5c9d42545393d118046504566b5657827e1a494b
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-16 15:55:31 +0900

    Fix: @Builder로 변경

--- src/main/java/com/commit/camp_reserve/service/UserServiceImpl.java
+++ src/main/java/com/commit/camp_reserve/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.commit.camp_reserve.service;
 
-import com.commit.camp_reserve.dto.SignUpUserInfo;
+import com.commit.camp_reserve.dto.SignUpUserRequest;
 import com.commit.camp_reserve.entity.User;
 import com.commit.camp_reserve.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
@@ -25,10 +28,21 @@ public class UserServiceImpl implements UserService{
 
     @Transactional
     @Override
-    public void signUpUser(SignUpUserInfo userInfo) {
+    public void signUpUser(SignUpUserRequest userRequest) {
 
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        User user = modelMapper.map(userInfo, User.class);
+        LocalDateTime currentTime = LocalDateTime.now();
+        User user = User.builder()
+                .email(userRequest.getEmail())
+                .name(userRequest.getName())
+                .password(userRequest.getPassword())
+                .nickname(userRequest.getNickname())
+                .birthDay(userRequest.getBirthDay())
+                .registrationDate(currentTime)
+                .enrollDate(currentTime)
+                .phoneNumber(userRequest.getPhoneNumber())
+                .userAddr(userRequest.getUserAddr())
+                .profileImageUrl(userRequest.getProfileImageUrl())
+                .build();
         userRepository.save(user);
     }
 }
