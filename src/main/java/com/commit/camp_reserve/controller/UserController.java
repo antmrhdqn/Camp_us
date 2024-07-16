d4c0a75dc5658864a92e73766b8999ff16687f2a
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-16 15:51:00 +0900

    Fix: api 수정

--- src/main/java/com/commit/camp_reserve/controller/UserController.java
+++ src/main/java/com/commit/camp_reserve/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.commit.camp_reserve.controller;
 
-import com.commit.camp_reserve.dto.SignUpUserInfo;
+import com.commit.camp_reserve.dto.SignUpUserRequest;
 import com.commit.camp_reserve.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/user")
+@RequestMapping("/v1/users")
 public class UserController {
 
     private final UserService userService;
@@ -20,9 +20,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("sign-up")
-    public ResponseEntity<Void> signUpUser(@RequestBody SignUpUserInfo userInfo) {
-        userService.signUpUser(userInfo);
+    @PostMapping("")
+    public ResponseEntity<Void> signUpUser(@RequestBody SignUpUserRequest userRequest) {
+        userService.signUpUser(userRequest);
         return ResponseEntity.ok().build();
     }
 
