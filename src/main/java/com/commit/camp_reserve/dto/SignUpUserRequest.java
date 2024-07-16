17126ae81156554920cb29d3d8697000fce3993e
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-16 15:49:57 +0900

    Fix: 클래스명 수정 및 컬럼 수정

--- src/main/java/com/commit/camp_reserve/dto/SignUpUserRequest.java
+++ src/main/java/com/commit/camp_reserve/dto/SignUpUserRequest.java
@@ -1,18 +1,20 @@
 package com.commit.camp_reserve.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
-public class SignUpUserInfo {
-    private int userId;
+public class SignUpUserRequest {
+    private Long userId;
     private String email;
     private String password;
     private String name;
     private String nickname;
     private String birthDay;
-    private String registrationDate;
-    private String withdrawDate;
-    private String withdrawStatus;
+    private LocalDateTime registrationDate;
+    private LocalDateTime enrollDate;
     private String phoneNumber;
     private String userAddr;
     private String profileImageUrl;
