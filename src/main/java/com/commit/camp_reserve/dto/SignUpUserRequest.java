f6e5e00637e50574f09122f98420246faa9b34b2
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-17 15:49:25 +0900

    Fix: 회원가입에 필요없는정보 삭제

--- src/main/java/com/commit/camp_reserve/dto/SignUpUserRequest.java
+++ src/main/java/com/commit/camp_reserve/dto/SignUpUserRequest.java
@@ -1,21 +1,14 @@
 package com.commit.camp_reserve.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 public class SignUpUserRequest {
-    private Long userId;
     private String email;
     private String password;
     private String name;
     private String nickname;
     private String birthDay;
-    private LocalDateTime registrationDate;
-    private LocalDateTime enrollDate;
     private String phoneNumber;
     private String userAddr;
-    private String profileImageUrl;
 }
