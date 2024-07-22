e33b35c2dbcf47e6b2a433481f10b56ba03e273e
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-22 15:11:06 +0900

    Chore: userId 자료형 수정

--- src/main/java/com/commit/campus/repository/UserRepository.java
+++ src/main/java/com/commit/campus/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.commit.campus.repository;
 import com.commit.campus.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByEmail(String email);
 
 }
