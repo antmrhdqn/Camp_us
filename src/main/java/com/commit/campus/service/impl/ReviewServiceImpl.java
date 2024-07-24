942008a5a4e750a368a22000560ef7b0f12b9d5a
antmrhdqn <antmrhdqn@gmail.com>
2024-07-24 16:07:19 +0900

    Feat: ReviewService 메소드 틀 추가

--- src/main/java/com/commit/campus/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/commit/campus/service/impl/ReviewServiceImpl.java
@@ -1,4 +1,31 @@
 package com.commit.campus.service.impl;
 
-public class ReviewServiceImpl {
+import com.commit.campus.dto.ReviewDTO;
+import com.commit.campus.entity.Review;
+import com.commit.campus.service.ReviewService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+    @Override
+    public Page<ReviewDTO> getReviewsByCampId(String campingSiteId, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Review createReview(ReviewDTO reviewDTO) {
+        return null;
+    }
+
+    @Override
+    public Review updateReview(String reviewId, ReviewDTO reviewDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteReview(String reviewId) {
+
+    }
 }
