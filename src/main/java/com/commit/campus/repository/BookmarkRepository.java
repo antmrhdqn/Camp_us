1d47655fba8f73aa1fd463750bc7e11ca1b93a22
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-25 15:32:27 +0900

    Feat: 찜 저장 기능 추가

--- src/main/java/com/commit/campus/repository/BookmarkRepository.java
+++ src/main/java/com/commit/campus/repository/BookmarkRepository.java
@@ -1,4 +1,25 @@
 package com.commit.campus.repository;
 
+
+import com.commit.campus.entity.Bookmark;
+import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+
+
+@Repository
 public class BookmarkRepository {
+
+    private final DynamoDbTable<Bookmark> bookmarkDynamoDBTable;
+
+    public BookmarkRepository(DynamoDbEnhancedClient dynamoDbEnhancedClient) {
+        this.bookmarkDynamoDBTable = dynamoDbEnhancedClient.table("Bookmark", TableSchema.fromBean(Bookmark.class));
+    }
+
+    public void save(Bookmark bookmark) {
+        bookmarkDynamoDBTable.putItem(bookmark);
+    }
+
+
 }
