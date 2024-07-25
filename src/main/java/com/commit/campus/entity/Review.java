2fcf9ace0e4f1febcd87b41389a0460d8a9de639
antmrhdqn <antmrhdqn@gmail.com>
2024-07-25 12:07:14 +0900

    Feat: Review 엔티티 작성

--- src/main/java/com/commit/campus/entity/Review.java
+++ src/main/java/com/commit/campus/entity/Review.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "riview")
 @Builder
@@ -17,5 +19,26 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "review_id")
-    private long reviewId;
+    private Long reviewId;
+
+    @Column(name = "camp_id", nullable = false)
+    private long campId;
+
+    @Column(name = "user_id", nullable = false)
+    private long userId;
+
+    @Column(name = "review_content", columnDefinition = "TEXT")
+    private String reviewContent;
+
+    @Column(name = "rating")
+    private Byte rating;
+
+    @Column(name = "review_created_date", nullable = false, updatable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime reviewCreatedDate;
+
+    @Column(name = "review_modification_date", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime reviewModificationDate;
+
+    @Column(name = "review_image_url", length = 255)
+    private String reviewImageUrl;
 }
