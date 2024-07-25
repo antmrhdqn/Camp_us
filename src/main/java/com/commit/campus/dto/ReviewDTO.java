306351a971b773c49c10a8bb2a8948ac10bb72ef
antmrhdqn <antmrhdqn@gmail.com>
2024-07-25 12:07:51 +0900

    Feat: ReviewDTO 작성

--- src/main/java/com/commit/campus/dto/ReviewDTO.java
+++ src/main/java/com/commit/campus/dto/ReviewDTO.java
@@ -1,10 +1,18 @@
 package com.commit.campus.dto;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 
-@Getter
-@Builder
-public class ReviewDTO {
+import java.time.LocalDateTime;
 
+@Data
+public class ReviewDTO {
+    private Long reviewId;
+    private Long campId;
+    private String reviewContent;
+    private int rating;
+    private LocalDateTime reviewCreatedDate;
+    private LocalDateTime reviewModificationDate;
+    private String reviewImageUrl;
 }
