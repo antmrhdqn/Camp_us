4cbd698a01e404c7085065bb52f192fb063cf184
bucky1005 <null00007@naver.com>
2024-07-25 18:27:21 +0900

    feat: ReservationController 초기 생성
    Fixes: #53

--- src/main/java/com/commit/campus/controller/ReservationController.java
+++ src/main/java/com/commit/campus/controller/ReservationController.java
@@ -1,5 +1,18 @@
 package com.commit.campus.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/reservations")
 public class ReservationController {
     // 예약 관련 기능
+
+    @GetMapping("/")
+    public ResponseEntity<Void> reservation() {
+
+        return ResponseEntity.ok().build();
+    }
 }
