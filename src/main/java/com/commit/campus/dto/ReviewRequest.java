5487b8d1f3bd51d7ddc98f3805ce99edc13fb31c
antmrhdqn <antmrhdqn@gmail.com>
2024-07-26 10:17:23 +0900

    Feat: ReviewRequest 쿨래스 작성

--- src/main/java/com/commit/campus/dto/ReviewRequest.java
+++ src/main/java/com/commit/campus/dto/ReviewRequest.java
@@ -4,4 +4,8 @@ import lombok.Data;
 
 @Data
 public class ReviewRequest {
+    private Long campId;
+    private String reviewContent;
+    private int rating;
+    private String reviewImageUrl;
 }
