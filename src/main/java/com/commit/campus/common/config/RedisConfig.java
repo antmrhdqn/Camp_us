0afd6ba27b9b9d9b899b72316c87212c72fb29e9
bucky1005 <null00007@naver.com>
2024-07-28 16:57:42 +0900

    init: redisConfig 추가
    Fixes: #67

---
+++ src/main/java/com/commit/campus/common/config/RedisConfig.java
@@ -0,0 +1,20 @@
+package com.commit.campus.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import io.lettuce.core.*;
+
+@Configuration
+public class RedisConfig {
+
+
+//    lettuce 사용
+//    RedisClient redisClient = RedisClient.create("redis://password@localhost:6379/0");
+//    StatefulRedisConnection<String, String> connection = redisClient.connect();
+//    RedisCommands<String, String> syncCommands = connection.sync();
+//
+//syncCommands.set("key","Hello, Redis!");
+//
+//connection.close();
+//redisClient.shutdown();
+
+}
