376b3b2b2a08d408d45e765d88872059b4aa6a0a
bucky1005 <null00007@naver.com>
2024-07-29 14:45:45 +0900

    feat: ReservationDTO 재정의
    RequestBody를 위한 reservationDTO 재정의
    
    Fixes: #53

--- src/main/java/com/commit/campus/dto/ReservationDTO.java
+++ src/main/java/com/commit/campus/dto/ReservationDTO.java
@@ -3,20 +3,21 @@ package com.commit.campus.dto;
 import com.commit.campus.entity.User;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
 @Builder
+@ToString
 public class ReservationDTO {
 
-    private int reservation_id;
-    private User user;
-//    private long camp_id;     // 캠핑장 아이디를 넣는 것이 좋을지 고민
-    private long camp_facs_id;
-    private LocalDateTime reservation_date;
-    private Date entry_date;
-    private Date leaving_date;
-    private String gear_rental_status;
+    private int userId;
+    private long campId;
+    private long campFacsId;
+    private LocalDateTime reservationDate;
+    private Date entryDate;
+    private Date leavingDate;
+    private String gearRentalStatus;
 }
