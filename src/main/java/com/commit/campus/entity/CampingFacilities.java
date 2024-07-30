8c6342636dd5e0777a01e6498cf1b35d13a9b14f
antmrhdqn <antmrhdqn@gmail.com>
2024-07-30 17:16:25 +0900

    Refactor: CampingFacilities 엔티티 수정

--- src/main/java/com/commit/campus/entity/CampingFacilities.java
+++ src/main/java/com/commit/campus/entity/CampingFacilities.java
@@ -9,26 +9,42 @@ import java.util.Date;
 public class CampingFacilities {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long campFacisId;  // 캠핑장 시설 ID
+    @Column(name = "camp_facs_id")
+    private long campFacsId;
 
-    @ManyToOne
-    @JoinColumn(name = "camp_id")
-    private Camping camping;  // 캠핑장 ID
+    @Column(name = "camp_id")
+    private long campId;
 
-    private String facsTypeId;  // 시설 유형 ID
-    private int innerFacs;  // 내부 시설 수
-    private int toiletCo;  // 화장실 수
-    private int swrmCo;  // 샤워실 수
-    private int wtrplCo;  // 식수대 수
-    private int brazierCo;  // 화로 수
-    private String options;  // 옵션
-    private String posblFcltyCl;  // 가능 시설 분류
-    private String animalCmgCl;  // 애완동물 출입 가능 여부
-    private String firstImageUrl;  // 첫 번째 이미지 URL
-    private Date createdTime;  // 생성 시간
-    private Date modifiedTime;  // 수정 시간
-    private String operDeCl;  // 운영 기간
-    private String trlerAcmpnyAt;  // 트레일러 동반 가능 여부
-    private String caravAcmpnyAt;  // 카라반 동반 가능 여부
-    private String dprtmLendCl;  // 부서 대여 분류
+    @Column(name = "facs_type_id")
+    private int facsTypeId;
+
+    @Column(name = "internal_facilities_list")
+    private String internalFacilitiesList;
+
+    @Column(name = "toilet_cnt")
+    private int toiletCnt;
+
+    @Column(name = "shower_room_cnt")
+    private int showerRoomCnt;
+
+    @Column(name = "sink_cnt")
+    private int sinkCnt;
+
+    @Column(name = "brazier_class")
+    private String brazierClass;
+
+    @Column(name = "personal_trailer_status")
+    private String personalTrailerStatus;
+
+    @Column(name = "personal_caravan_status")
+    private String personalCaravanStatus;
+
+    // fk
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "camp_id", insertable = false, updatable = false)
+    private Camping campingEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "facs_type_id", insertable = false, updatable = false)
+    private FacilityType facilityTypeEntity;
 }
