9aa0180c991df41f952d8724868d58a9e909da12
bucky1005 <null00007@naver.com>
2024-07-31 11:17:09 +0900

    style: LettuceConfig 주석 추가
    - 주석 추가
    
    Fixes: #53, #87

--- src/main/java/com/commit/campus/common/config/LettuceConfig.java
+++ src/main/java/com/commit/campus/common/config/LettuceConfig.java
@@ -2,6 +2,7 @@ package com.commit.campus.common.config;
 
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
 import io.lettuce.core.api.sync.RedisCommands;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,16 +21,19 @@ public class LettuceConfig {
 
     @Bean
     public RedisClient redisClient() {
-        return RedisClient.create("redis://localhost:6379");
+        // Redis 서버 URL 설정
+        return RedisClient.create("redis://localhost:"+ port);
     }
 
     @Bean
     public StatefulRedisConnection<String, String> redisConnection(RedisClient redisClient) {
+        // Redis 연결 생성
         return redisClient.connect();
     }
 
     @Bean
-    public RedisCommands<String, String> redisCommands(StatefulRedisConnection<String, String> connection) {
-        return connection.sync();
+    public RedisAsyncCommands<String, String> redisAsyncCommands(StatefulRedisConnection<String, String> connection) {
+        // 비동기 명령어 객체 생성
+        return connection.async();      // 동기인 경우 connection.sync();
     }
 }
