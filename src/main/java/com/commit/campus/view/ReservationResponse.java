693b3855012b2cb5b8a85b62a9bf51bf6a90f0d9
bucky1005 <null00007@naver.com>
2024-07-31 16:20:45 +0900

    feat: 예약 확정 시 전달할 response View 설정
    Fixes: #53, #87

--- src/main/java/com/commit/campus/view/ReservationResponse.java
+++ src/main/java/com/commit/campus/view/ReservationResponse.java
@@ -1,6 +1,11 @@
 package com.commit.campus.view;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class ReservationResponse {
-
-
+    private String reservationId;
+    private String campName;
+    private Date entryDate;
+    private Date leavingDate;
 }
