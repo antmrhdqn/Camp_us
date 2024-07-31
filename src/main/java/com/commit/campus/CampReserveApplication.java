46e8253411825f4d751a100b8d01a7753b243b08
bucky1005 <null00007@naver.com>
2024-07-31 16:34:02 +0900

    style: 임시로 설정해 둔 주석 해제
    Fixes: #53, #87

--- src/main/java/com/commit/campus/CampReserveApplication.java
+++ src/main/java/com/commit/campus/CampReserveApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.commit.campus.common.openfeign")
