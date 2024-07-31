7f1716fb84074ee8ceebcba04c27a900838195db
antmrhdqn <antmrhdqn@gmail.com>
2024-07-31 09:57:03 +0900

    Feat: ReviewView 작성

--- src/main/java/com/commit/campus/view/ReviewView.java
+++ src/main/java/com/commit/campus/view/ReviewView.java
@@ -1,4 +1,10 @@
 package com.commit.campus.view;
 
+import lombok.Data;
+
+@Data
 public class ReviewView {
+    private String reviewContent;
+    private byte rating;
+    private String reviewImageUrl;
 }
