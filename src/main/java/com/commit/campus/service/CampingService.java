f90de56f3dc92802c85eda1ea5dabbde55f66ee1
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-31 12:38:27 +0900

    Feat: 북마크된 캠핑장정보 단일조회 기능 추가

--- src/main/java/com/commit/campus/service/CampingService.java
+++ src/main/java/com/commit/campus/service/CampingService.java
@@ -1,5 +1,6 @@
 package com.commit.campus.service;
 
+import com.commit.campus.dto.BookmarkedCampingDTO;
 import com.commit.campus.dto.CampingDTO;
 
 import java.util.List;
@@ -7,4 +8,5 @@ import java.util.List;
 public interface CampingService {
     List<CampingDTO> getAllCampings();
     CampingDTO createCamping(CampingDTO campingDTO);
+    BookmarkedCampingDTO getBookmarkedCamping(Long campId);
 }
