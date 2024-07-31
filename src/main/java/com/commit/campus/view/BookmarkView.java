b475286ab2da3a6f842381f8c629f0d22a93de5d
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-31 12:45:29 +0900

    Feat: 출력용 BookmarkView 추가

--- src/main/java/com/commit/campus/view/BookmarkView.java
+++ src/main/java/com/commit/campus/view/BookmarkView.java
@@ -6,7 +6,21 @@ import lombok.Getter;
 @Builder
 @Getter
 public class BookmarkView {
-    private Long userId;
-    private Long campId;
 
+    private String campName;
+    private String doName;
+    private String sigunguName;
+    private String postCode;
+    private String induty;
+    private String firstImageUrl;
+
+    @Builder
+    public BookmarkView(String campName, String doName, String sigunguName, String postCode, String induty, String firstImageUrl) {
+        this.campName = campName;
+        this.doName = doName;
+        this.sigunguName = sigunguName;
+        this.postCode = postCode;
+        this.induty = induty;
+        this.firstImageUrl = firstImageUrl;
+    }
 }
