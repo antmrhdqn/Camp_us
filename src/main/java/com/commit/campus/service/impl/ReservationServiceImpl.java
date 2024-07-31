540c31529dcb0660c12294a443d63b8d55f3f366
bucky1005 <null00007@naver.com>
2024-07-31 10:10:25 +0900

    feat: ReservationId 생성 로직 추가
    - Redis에서 해쉬키로 사용할 RervationId 생성 로직 추가
    
    Fixes: #53, #87

--- src/main/java/com/commit/campus/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/commit/campus/service/impl/ReservationServiceImpl.java
@@ -3,34 +3,67 @@ package com.commit.campus.service.impl;
 import com.commit.campus.dto.ReservationDTO;
 import com.commit.campus.repository.ReservationRepository;
 import com.commit.campus.service.ReservationService;
-import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.async.RedisAsyncCommands;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+@Slf4j
 @Service
 public class ReservationServiceImpl implements ReservationService {
 
     private final ReservationRepository reservationRepository;
-    private RedisCommands<String, String> redisCommands;
+    private final RedisClient redisClient;
+    private final RedisAsyncCommands redisAsyncCommands;
+
+    private static long index = 1;
 
     @Autowired
-    public ReservationServiceImpl(ReservationRepository reservationRepository, RedisCommands<String, String> redisCommands) {
+    public ReservationServiceImpl(ReservationRepository reservationRepository,
+                                  RedisClient redisClient,
+                                  RedisAsyncCommands redisAsyncCommands) {
         this.reservationRepository = reservationRepository;
-        this.redisCommands = redisCommands;
+        this.redisClient = redisClient;
+        this.redisAsyncCommands = redisAsyncCommands;
     }
 
     @Override
     public void createReservation(ReservationDTO reservationDTO) {
-        // Generate a unique key for the reservation
-        String reservationKey = generateReservationKey(reservationDTO);
 
-        String reservationValue = serializeToJson(reservationDTO);
+        // 예약 아이디 생성
+        LocalDateTime reservationDate = reservationDTO.getReservationDate();
+        String reservationId = createReservationId(reservationDate);
 
-        // Save the reservation request to Redis with TTL of 1 hour
-        redisCommands.set(reservationKey, reservationValue);
-//        redisCommands.expire(reservationKey, 1, TimeUnit.HOURS);
+        String key = "reservation:" + reservationId;
+
+        redisAsyncCommands.hset(key, "reservationId", reservationId);
+        redisAsyncCommands.hset(key, "user", reservationDTO.getUserId());
+        redisAsyncCommands.hset(key, "campId", reservationDTO.getCampId());
+        redisAsyncCommands.hset(key, "campFacsId", reservationDTO.getCampFacsId());
+        redisAsyncCommands.hset(key, "reservationDate", reservationDTO.getReservationDate());
+        redisAsyncCommands.hset(key, "entryDate", reservationDTO.getEntryDate());
+        redisAsyncCommands.hset(key, "leavingDate", reservationDTO.getLeavingDate());
+        redisAsyncCommands.hset(key, "gearRentalStatus", reservationDTO.getGearRentalStatus());
+
+        log.info("redis에 예약 내역 저장 완료");
+    }
+
+    // redis에 해쉬 키로 저장할 예약아이디 생성 (예약일자 + 6자리 인덱스값)
+    private synchronized String createReservationId(LocalDateTime reservationDate) {
+
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String formattedDate = reservationDate.format(dateFormat);
+
+        String indexCode = String.format("%06d", index);
+        index ++;
+
+        String reservationId = formattedDate + indexCode;
+
+        return reservationId;
     }
 
     private String generateReservationKey(ReservationDTO reservationDTO) {
