c3d684167bf1e3a04c9b2a6cc0b9bef5656672da
bucky1005 <null00007@naver.com>
2024-07-31 15:39:07 +0900

    refactor: createReservation 메소드에 반환값 추가
    - ReservationId를 반환하도록 수정
    
    Fixes: #53, #87

--- src/main/java/com/commit/campus/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/commit/campus/service/impl/ReservationServiceImpl.java
@@ -5,12 +5,15 @@ import com.commit.campus.repository.ReservationRepository;
 import com.commit.campus.service.ReservationService;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.async.RedisAsyncCommands;
+import io.lettuce.core.api.sync.RedisCommands;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -19,38 +22,83 @@ public class ReservationServiceImpl implements ReservationService {
     private final ReservationRepository reservationRepository;
     private final RedisClient redisClient;
     private final RedisAsyncCommands redisAsyncCommands;
+    private final RedisCommands redisCommands;
 
     private static long index = 1;
 
     @Autowired
     public ReservationServiceImpl(ReservationRepository reservationRepository,
                                   RedisClient redisClient,
-                                  RedisAsyncCommands redisAsyncCommands) {
+                                  RedisAsyncCommands redisAsyncCommands,
+                                  RedisCommands redisCommands) {
         this.reservationRepository = reservationRepository;
         this.redisClient = redisClient;
         this.redisAsyncCommands = redisAsyncCommands;
+        this.redisCommands = redisCommands;
     }
 
     @Override
-    public void createReservation(ReservationDTO reservationDTO) {
+    public String createReservation(ReservationDTO reservationDTO) {
 
         // 예약 아이디 생성
         LocalDateTime reservationDate = reservationDTO.getReservationDate();
         String reservationId = createReservationId(reservationDate);
 
-        String key = "reservation:" + reservationId;
+        String key = "reservationInfo:" + reservationId;
 
-        // 저장 실패한 경우 예외 처리 추가하기
-        redisAsyncCommands.hset(key, "reservationId", reservationId);
-        redisAsyncCommands.hset(key, "user", reservationDTO.getUserId());
-        redisAsyncCommands.hset(key, "campId", reservationDTO.getCampId());
-        redisAsyncCommands.hset(key, "campFacsId", reservationDTO.getCampFacsId());
-        redisAsyncCommands.hset(key, "reservationDate", reservationDTO.getReservationDate());
-        redisAsyncCommands.hset(key, "entryDate", reservationDTO.getEntryDate());
-        redisAsyncCommands.hset(key, "leavingDate", reservationDTO.getLeavingDate());
-        redisAsyncCommands.hset(key, "gearRentalStatus", reservationDTO.getGearRentalStatus());
+        /* 궁금증.
+            비동기식으로 데이터를 저장한다고 하면, createReservation 요청이 동시에 여러개 날아왔을 때
+            redis에 요청 건 별로 데이터가 차곡차곡 저장되는 것이 아니라
+            별다른 순서 없이 뒤죽박죽 데이터가 쌓이게 되는 것인지??
 
-        log.info("redis에 예약 내역 저장 완료");
+            위 가정이 맞다면
+            데이터를 찾아올 때에는 해쉬키를 통해서 데이터를 찾아오게 되니 뒤죽박죽 저장되어도 상관 없는 것인지?
+        */
+
+        // 동기식으로 데이터 저장
+        try {
+            redisCommands.hset(key, "reservationId", reservationId);
+            redisCommands.hset(key, "user", reservationDTO.getUserId());
+            redisCommands.hset(key, "campId", reservationDTO.getCampId());
+            redisCommands.hset(key, "campFacsId", reservationDTO.getCampFacsId());
+            redisCommands.hset(key, "reservationDate", reservationDTO.getReservationDate());
+            redisCommands.hset(key, "entryDate", reservationDTO.getEntryDate());
+            redisCommands.hset(key, "leavingDate", reservationDTO.getLeavingDate());
+            redisCommands.hset(key, "gearRentalStatus", reservationDTO.getGearRentalStatus());
+
+            log.info("redis에 예약 내역 저장 완료");
+
+        } catch (RuntimeException e) {
+            throw new RuntimeException("redis에 저장이 되지 않음");
+        }
+
+        return reservationId;
+    }
+
+    @Override
+    public ReservationDTO confirmReservation(String reservationId) {
+
+//        String key = "reservationInfo:" + reservationId;
+//
+//        Map<String, String> reservationInfo = redisCommands.hgetall(key);
+//
+//        if (reservationInfo == null) {
+//            return null;
+//        }
+//
+//        reservationInfo.forEach((k, value) -> System.out.println("Key: " + k + ", Value: " + value));
+
+//        ReservationDTO reservationDTO = new ReservationDTO();
+//        reservationDTO.setReservationId(reservationId);
+//        reservationDTO.setUserId(reservationInfo.get());
+//        reservationDTO.setCampId(reservationDTO.getCampId());
+//        reservationDTO.setCampFacsId(reservationDTO.getCampFacsId());
+//        reservationDTO.setReservationDate(reservationDTO.getReservationDate());
+//        reservationDTO.setEntryDate(reservationDTO.getEntryDate());
+//        reservationDTO.setLeavingDate(reservationDTO.getLeavingDate());
+//        reservationDTO.getGearRentalStatus(reservationDTO.getGearRentalStatus());
+
+        return null;
     }
 
     // redis에 해쉬 키로 저장할 예약아이디 생성 (예약일자 + 6자리 인덱스값)
