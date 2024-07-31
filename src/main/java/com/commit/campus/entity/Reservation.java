9feb1b61aaa37960a6b0aa3b9cd310ff5b088b13
bucky1005 <null00007@naver.com>
2024-07-31 20:21:15 +0900

    refactor: GeneratedValue 속성 제거
    - reservationId에 auto increment 속성을 제외하기 위해 해당 속성을 제거함
    
    Resolves: #106, #107

--- src/main/java/com/commit/campus/entity/Reservation.java
+++ src/main/java/com/commit/campus/entity/Reservation.java
@@ -11,9 +11,16 @@ import java.util.Date;
 @Getter
 public class Reservation implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int reservationId;  // 예약 ID
+    private long campId;        // 예약한 캠핑장
+    private long campFacsId;    // 예약한 시설
+    private LocalDateTime reservationDate;  // 예약 날짜
+    private Date entryDate;  // 입실 날짜
+    private Date leavingDate;  // 퇴실 날짜
+    private String reservationStatus;  // 예약 상태
+    private String gearRentalStatus;  // 장비 대여 상태
 
+    // fk
     @ManyToOne
     @JoinColumn(name = "camp_facs_id", insertable = false, updatable = false)
     private CampingFacilities campingFacilities;  // 캠핑장 시설 ID
@@ -21,10 +28,4 @@ public class Reservation implements Serializable {
     @ManyToOne
     @JoinColumn(name = "user_id", insertable = false, updatable = false)
     private User user;  // 사용자 ID
-
-    private LocalDateTime reservationDate;  // 예약 날짜
-    private Date entryDate;  // 입실 날짜
-    private Date leavingDate;  // 퇴실 날짜
-    private String reservationStatus;  // 예약 상태
-    private String gearRentalStatus;  // 장비 대여 상태
 }
