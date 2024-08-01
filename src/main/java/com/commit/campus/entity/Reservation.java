c8a049e79c8d792ff909c470418622280646bff8
bucky1005 <null00007@naver.com>
2024-08-01 16:31:00 +0900

    fix: Entity의 multiple logical column names 에러 해결
    - @Column(name = "") 설정하여 table과 entity의 컬럼명 명시적으로 매칭
    
    Resolves: #106, #107

--- src/main/java/com/commit/campus/entity/Reservation.java
+++ src/main/java/com/commit/campus/entity/Reservation.java
@@ -11,21 +11,39 @@ import java.util.Date;
 @Getter
 public class Reservation implements Serializable {
     @Id
-    private int reservationId;  // 예약 ID
+    @Column(name = "reservation_id")
+    private long reservationId;  // 예약 ID
+
+    @Column(name = "user_id")
+    private long userId;       // 유저 id
+
+    @Column(name = "camp_id")
     private long campId;        // 예약한 캠핑장
+
+    @Column(name = "camp_facs_id")
     private long campFacsId;    // 예약한 시설
+
+    @Column(name = "reservation_date")
     private LocalDateTime reservationDate;  // 예약 날짜
+
+    @Column(name = "entry_date")
     private Date entryDate;  // 입실 날짜
+
+    @Column(name = "leaving_date")
     private Date leavingDate;  // 퇴실 날짜
+
+    @Column(name = "reservation_status")
     private String reservationStatus;  // 예약 상태
+
+    @Column(name = "gear_rental_status")
     private String gearRentalStatus;  // 장비 대여 상태
 
     // fk
     @ManyToOne
     @JoinColumn(name = "camp_facs_id", insertable = false, updatable = false)
-    private CampingFacilities campingFacilities;  // 캠핑장 시설 ID
+    private CampingFacilities campingFacilities;
 
     @ManyToOne
     @JoinColumn(name = "user_id", insertable = false, updatable = false)
-    private User user;  // 사용자 ID
+    private User user;
 }
