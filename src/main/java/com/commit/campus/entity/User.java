a920259b5a73e410ff8b9e1e5b392f22c8df6370
bucky1005 <null00007@naver.com>
2024-08-02 18:05:26 +0900

    refactor: User entity password 필드의 컬럼명 매칭값 변경(password -> passwd)
    - User 테이블의 비밀번호 컬럼명이 passwd로 생성되어 있어 변경하였음.
    
    Fixes: #106, #107

--- src/main/java/com/commit/campus/entity/User.java
+++ src/main/java/com/commit/campus/entity/User.java
@@ -24,7 +24,7 @@ public class User implements Serializable {
     @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "passwd", nullable = false)
     private String password;
 
     @Column(name = "name", nullable = false)
