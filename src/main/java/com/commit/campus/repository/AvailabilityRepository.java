6819124e251548f0e4de134a6f54c052f9fbb6d2
bucky1005 <null00007@naver.com>
2024-08-05 10:49:56 +0900

    feat: findByCampIdAndDate 추가
    Fixes: #106, #107

--- src/main/java/com/commit/campus/repository/AvailabilityRepository.java
+++ src/main/java/com/commit/campus/repository/AvailabilityRepository.java
@@ -3,6 +3,8 @@ package com.commit.campus.repository;
 import com.commit.campus.entity.Availability;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
+import java.util.Date;
 
+public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
+    Availability findByCampIdAndDate(Long campId, Date date);
 }
