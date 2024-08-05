c75bb80d54aaff06923b33430be71670e9c54f2e
bucky1005 <null00007@naver.com>
2024-08-05 10:48:52 +0900

    feat: AvailabilityRepository 생성
    Fixes: #106, #107

---
+++ src/main/java/com/commit/campus/repository/AvailabilityRepository.java
@@ -0,0 +1,8 @@
+package com.commit.campus.repository;
+
+import com.commit.campus.entity.Availability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
+
+}
