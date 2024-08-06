ebe83ded758f0ea03a7a91962f5094da4b46f14b
antmrhdqn <antmrhdqn@gmail.com>
2024-08-06 10:23:23 +0900

    Refactor: GlobalExceptionAdvice 수정 - 클래스명 handler -> advice - 별도의 advice 패키지 생성 후 이동 - @ControllerAdvice -> @RestControllerAdvice

---
+++ src/main/java/com/commit/campus/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,29 @@
+package com.commit.campus.advice;
+
+import com.commit.campus.common.exceptions.NotAuthorizedException;
+import com.commit.campus.common.exceptions.ReviewAlreadyExistsException;
+import com.commit.campus.view.ErrorView;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler(ReviewAlreadyExistsException.class)
+    public ResponseEntity<ErrorView> handleReviewAlreadyExists(ReviewAlreadyExistsException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorView("REVIEW_ALREADY_EXISTS", ex.getMessage()));
+    }
+
+    @ExceptionHandler(NotAuthorizedException.class)
+    public ResponseEntity<ErrorView> handleNotAuthorizedException(NotAuthorizedException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ErrorView("NOT_AUTHORIZED", ex.getMessage()));
+    }
+}
