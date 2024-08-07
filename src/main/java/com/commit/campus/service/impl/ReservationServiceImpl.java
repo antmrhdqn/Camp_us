4e8a061deb19ba8c9dc642943673139582af54bd
bucky1005 <null00007@naver.com>
2024-08-07 09:04:49 +0900

    refactor: dateEquals 메소드에서 Calendar 함수 제거 -> DateFormatter 사용
    Fixes: #106, #107

--- src/main/java/com/commit/campus/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/commit/campus/service/impl/ReservationServiceImpl.java
@@ -272,13 +272,10 @@ public class ReservationServiceImpl implements ReservationService {
 
     // 날짜 비교 메소드
     public boolean dateEquals(Date date1, Date date2) {
-        Calendar cal1 = Calendar.getInstance();
-        Calendar cal2 = Calendar.getInstance();
-        cal1.setTime(date1);
-        cal2.setTime(date2);
-        return (cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)) &&
-                (cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH)) &&
-                (cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String dateStr1 = formatter.format(date1.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        String dateStr2 = formatter.format(date2.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        return dateStr1.equals(dateStr2);
     }
 
     private Date addDays(Date date, int days) {
