bee9fd4a4695fb045bf9fcde5978bcfc777900ef
bucky1005 <null00007@naver.com>
2024-08-08 13:36:23 +0900

    refactor: AvailabilityRepository의 Date -> LocalDate로 변경
    Fixes: #106, #107

--- src/main/java/com/commit/campus/repository/AvailabilityRepository.java
+++ src/main/java/com/commit/campus/repository/AvailabilityRepository.java
@@ -5,13 +5,13 @@ import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
     @Query("SELECT a FROM Availability a WHERE a.campId = :campId AND a.date BETWEEN :startDate AND :endDate")
-    List<Availability> findByCampIdAndDateBetween(@Param("campId") Long campId, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
+    List<Availability> findByCampIdAndDateBetween(@Param("campId") Long campId, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
     @Query(value = "SELECT * FROM Availability a WHERE a.camp_id = :campId AND DATE(a.date) = DATE(:date)", nativeQuery = true)
-    Availability findByCampIdAndDate(@Param("campId") Long campId, @Param("date") Date date);
+    Availability findByCampIdAndDate(@Param("campId") Long campId, @Param("date") LocalDate date);
 }
