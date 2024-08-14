93f61c6167856f71d805cfb520774f2b294d310a
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-08-14 14:33:57 +0900

    Chore: @NoArgsConstructor 추가

--- src/main/java/com/commit/campus/dto/SignUpUserRequest.java
+++ src/main/java/com/commit/campus/dto/SignUpUserRequest.java
@@ -1,10 +1,13 @@
 package com.commit.campus.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
 public class SignUpUserRequest {
     private String email;
     private String password;
